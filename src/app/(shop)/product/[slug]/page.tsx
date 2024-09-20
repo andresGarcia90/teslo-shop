559cb2e597403ea96cebb1b2a5ecbf387ccad4cc
Andres Garcia <amgarciaamado@hipotecarioseguros.com>
2024-09-20 19:36:50 -0300

    Adding metadata to the product page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,31 +1,69 @@
 export const revalidate = 604800; //7 días
 
-import { getProductBySlug } from '@/app/actions/products/product-slug';
-import { SizeSelector, QuantitySelector, ProductMobileSlideshow, ProductSlideshow, StockLabel } from '@/components';
+import type { Metadata, ResolvingMetadata } from 'next';
 import { titleFont } from '@/config/fonts';
-import { notFound } from 'next/navigation';
-import React from 'react'
 
-interface Props{
+import { getProductBySlug } from '@/app/actions/products/product-slug';
+
+import { notFound } from 'next/navigation';
+
+import {
+  SizeSelector,
+  QuantitySelector,
+  ProductMobileSlideshow,
+  ProductSlideshow,
+  StockLabel,
+} from '@/components';
+
+interface Props {
   params: {
     slug: string;
-  }
+  };
 }
 
-const ProductPage = async ({ params }: Props ) => {
+// export const metadata: Metadata = {
+//   title: `${slug}`,
+//   description: '...',
+// }
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = params;
+  // fetch data
+  const product = await getProductBySlug({ slug });
+
+  return {
+    title: `${product?.title ?? 'producto no encontrado'} | Teslo Shop`,
+    description: product?.description ?? '',
+    keywords: product?.tags.join(', '),
+    openGraph: {
+      title: `${product?.title ?? 'producto no encontrado'} | Teslo Shop`,
+      description: product?.description ?? '',
+      images: `/products/${product?.images[0]}`,
+    },
+  };
+}
+
+const ProductPage = async ({ params }: Props) => {
   const { slug } = params;
   const product = await getProductBySlug({ slug });
-  
-  if(!product) notFound()
-  
+
+  if (!product) notFound();
+
   return (
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
       {/* Slideshow DK and MB*/}
       <div className="col-span-1 md:col-span-2 bg-gray-200">
-        <ProductSlideshow images={product.images} title={product.title} className='hidden md:block' />
-        <ProductMobileSlideshow images={product.images} title={product.title} className='block md:hidden'/>
+        <ProductSlideshow
+          images={product.images}
+          title={product.title}
+          className="hidden md:block"
+        />
+        <ProductMobileSlideshow
+          images={product.images}
+          title={product.title}
+          className="block md:hidden"
+        />
       </div>
-
 
       {/* Detalles */}
       <div className="col-span-1 px-5">
@@ -36,22 +74,21 @@ const ProductPage = async ({ params }: Props ) => {
         <p className="text-lg mb-5">${product.price}</p>
 
         {/* selector de tallas */}
-        <SizeSelector allSizes={product.sizes} selectedSize={product.sizes[0]} />
+        <SizeSelector
+          allSizes={product.sizes}
+          selectedSize={product.sizes[0]}
+        />
         {/* selector de cantidad */}
-        <QuantitySelector quantity={1}/>
+        <QuantitySelector quantity={1} />
         {/* Botones */}
-        <button className="btn-primary my-5">
-          Add to cart
-        </button>
+        <button className="btn-primary my-5">Add to cart</button>
 
         {/* Descripción */}
-        <h3 className='font-bold text-sm mb-3'>Description</h3>
+        <h3 className="font-bold text-sm mb-3">Description</h3>
         <p>{product.description}</p>
       </div>
-      
-
     </div>
-  )
-}
+  );
+};
 
-export default ProductPage
+export default ProductPage;
