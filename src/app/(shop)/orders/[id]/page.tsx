cc0a97c69a7cb0a1b16f7448938cc7ec6ef47199
Andres Garcia <garcia.andi90@gmail.com>
2024-10-16 14:37:27 -0300

    Adding Info for Orders

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,99 +1,108 @@
-import { Title } from '@/components';
-import { initialData } from '@/seed/seed';
+import { getOrderById } from '@/app/actions/order/get-order-by-id';
+
 import clsx from 'clsx';
 import Image from 'next/image';
-import React from 'react'
+
+import { Title } from '@/components';
 import { IoCardOutline } from 'react-icons/io5';
 
-interface Props{
+interface Props {
   params: {
     id: string
   }
 }
 
 
-const OrderPage = ({ params }: Props) => {
+const OrderPage = async ({ params }: Props) => {
   const { id } = params;
-  const products = initialData.products.slice(0, 4);
+  const { order, ok } = await getOrderById(id);
+  if (!ok) {
+    return (
+      <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+        Try again in a few minutes
+      </div>
+    )
+  }
 
+  const address = order!.OrderAddress;
+  
 
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
 
       <div className="flex flex-col w-[1000px]">
 
-        <Title title={ `Orden #${ id }` } />
+        <Title title={`Orden #${order!.id}`} />
 
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
 
-          {/* Carrito */ }
+          {/* Carrito */}
           <div className="flex flex-col mt-5">
 
             <div className={
               clsx(
                 "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
                 {
-                  'bg-red-500': false,
-                  'bg-green-700': true,
+                  'bg-red-500': !order!.isPaid,
+                  'bg-green-700': order!.isPaid,
                 }
               )
             }>
-              <IoCardOutline size={ 30 } />
-              {/* <span className="mx-2">Pendiente de pago</span> */ }
-              <span className="mx-2">Pagada</span>
+              <IoCardOutline size={30} />
+              {/* <span className="mx-2">Pendiente de pago</span> */}
+              <span className="mx-2">{order!.isPaid ? 'Pagada' : 'Pendiente de pago'}</span>
             </div>
 
 
 
-            {/* Items */ }
+            {/* Items */}
             {
-              products.map( product => (
+              order!.OrderItem.map(product => (
 
-                <div key={ product.slug } className="flex mb-5">
+                <div key={product.product.slug+product.size} className="flex mb-5">
                   <Image
-                    src={ `/products/${ product.images[ 0 ] }` }
-                    width={ 100 }
-                    height={ 100 }
-                    style={ {
+                    src={`/products/${product.product.ProductImages[0].url}`}
+                    width={100}
+                    height={100}
+                    style={{
                       width: '100px',
                       height: '100px'
-                    } }
-                    alt={ product.title }
+                    }}
+                    alt={product.product.slug}
                     className="mr-5 rounded"
                   />
 
                   <div>
-                    <p>{ product.title }</p>
-                    <p>${ product.price } x 3</p>
-                    <p className="font-bold">Subtotal: ${ product.price * 3 }</p>
+                    <p>{product.product.slug}</p>
+                    <p>{product.size}</p>
+                    <p>${product.price} x {product.quantity}</p>
+                    <p className="font-bold">Subtotal: ${product.price * product.quantity}</p>
                   </div>
 
                 </div>
 
 
-              ) )
+              ))
             }
           </div>
 
 
 
 
-          {/* Checkout - Resumen de orden */ }
+          {/* Checkout - Resumen de orden */}
           <div className="bg-white rounded-xl shadow-xl p-7">
 
             <h2 className="text-2xl mb-2">Dirección de entrega</h2>
             <div className="mb-10">
-              <p className="text-xl">Fernando Herrera</p>
-              <p>Av. Siempre viva 123</p>
-              <p>Col. Centro</p>
-              <p>Alcaldía Cuauhtémoc</p>
-              <p>Ciudad de México</p>
-              <p>CP 123123</p>
-              <p>123.123.123</p>
+              <p className="text-xl">{address!.firstName} {address!.lastName}</p>
+              <p>{address!.address}</p>
+              <p>{address!.city}</p>
+              <p>{address!.phone}</p>
+              <p>{address!.postalCode}</p>
             </div>
 
-            {/* Divider */ }
+            {/* Divider */}
             <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
 
 
@@ -122,14 +131,14 @@ const OrderPage = ({ params }: Props) => {
                 clsx(
                   "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
                   {
-                    'bg-red-500': false,
-                    'bg-green-700': true,
+                    'bg-red-500': !order!.isPaid,
+                    'bg-green-700': order!.isPaid,
                   }
                 )
               }>
-                <IoCardOutline size={ 30 } />
-                {/* <span className="mx-2">Pendiente de pago</span> */ }
-                <span className="mx-2">Pagada</span>
+                <IoCardOutline size={30} />
+                {/* <span className="mx-2">Pendiente de pago</span> */}
+                <span className="mx-2">{order!.isPaid ? 'Pagada' : 'Pendiente de pago'}</span>
               </div>
 
             </div>
@@ -148,7 +157,7 @@ const OrderPage = ({ params }: Props) => {
 
     </div>
   );
-  
+
 }
 
 export default OrderPage
